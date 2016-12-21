e3acd4ae2198fcd284dc69e0570a311f139f473b
Melissa Jaen <melissa.jaen@gmail.com>
2016-12-21 15:17:05 -0600

    Checkpoint 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,24 @@
-var animatePoints = function () {        
-    var points = document.getElementsByClassName('point');
-            
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.mtransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-             
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }          
+var pointsArray = document.getElementsByClassName('point')
+
+var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.mtransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+
+var animatePoints = function(points){   
+    forEach(points, revealPoint);        
 }; 
+
+window.onload = function () {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scrolls', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) { animatePoints(pointsArray);
+        }
+    });
+}
