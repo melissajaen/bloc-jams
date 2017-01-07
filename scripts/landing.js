97766a8ea1b1c8d0e71d2e143d9d376b1f9f671d
Melissa Jaen <melissa.jaen@gmail.com>
2017-01-07 17:12:05 -0600

    checkpoint 23 fixed

--- scripts/landing.js
+++ scripts/landing.js
@@ -17,8 +17,8 @@ window.onload = function () {
     }
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scrolls', function(event) {
+    window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) { animatePoints(pointsArray);
         }
     });
-} 
+}
