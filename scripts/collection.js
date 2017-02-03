19722d53b9e0d999a9257653b5f7b945c572b400
Melissa Jaen <melissa.jaen@gmail.com>
2017-02-02 23:33:25 -0600

    checkpoint 30

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,7 +1,8 @@
-var collectionItemTemplate =
-    '<div class="collection-album-container column fourth">'
+var buildCollectionItemTemplate = function() {
+    var template = 
+        '<div class="collection-album-container column fourth">'
 +    '   <img src="assets/images/album_covers/01.png"/>'
-+   '   <div class="collection-album-info caption">'
++    '     <div class="collection-album-info caption">'
 +    '       <p>'
 +    '       <a class="album-name" href="album.html"> The Colors </a>'
 +    '       <br/>'
@@ -13,11 +14,12 @@ var collectionItemTemplate =
 +    '   </div>'
 +    '   </div>'
     ;
+    return $(template);
 
-+ window.onload = function() {
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    collectionContainer.innerHTML = '';
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
     for (var i = 0; i <12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
-    }
+        var $newThumbnail = buildCollectionItemTemplate()};
+    $collectionContainer.append($newThumbnail);
 }
